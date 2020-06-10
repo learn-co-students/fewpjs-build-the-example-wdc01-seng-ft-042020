7b2b999dcb0f3e2e52a967b58a700c5b5a868550
agdsarge <agdsargeant@gmail.com>
2020-06-10 19:33:00 -0400

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
 
+document.querySelector('#modal').className = "hidden"
+
+const mdl = () => document.querySelector("#modal")
+// mdl().className = "hidden"
+
+document.addEventListener("DOMContentLoaded", () => {
+    //mdl().className = "hidden";
+    let hearts = document.querySelectorAll(".like-glyph")
+    hearts.forEach(n => {n.addEventListener("click", handleLikeClick)})
+
+})
+
+function handleLikeClick(e) {
+    console.log(e.target)
+
+    mimicServerCall()
+    .then(resp => {
+        if (e.target.innerText == EMPTY_HEART) {
+            e.target.innerText = FULL_HEART
+            e.target.className = "activated-heart"
+        }
+        else {
+            e.target.innerText = EMPTY_HEART
+            e.target.className = ""
+        }
+    })
+    .catch((err) => {
+        mdl().className = ''
+        mdl().lastElementChild.innerText = err
+        setTimeout(() => {mdl().className = 'hidden'}, 5000)
+    }
+    )
+}
 
 
 
